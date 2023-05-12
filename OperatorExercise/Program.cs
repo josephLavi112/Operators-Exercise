21c9331c986448061cb8aa80d938611a5c5d0233
Joseph LaVia <josephdlavia@gmail.com>
2023-05-12 14:59:54 -0500

    Operators math exercie

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+
+                Console.WriteLine(a + " / " + b + " is " +  quotient + " remainder " +  remainder);
+
+            Console.WriteLine("What is the radius of your circle?");
+             
+
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            var area = (Math.PI * Math.Pow(radius,2));
+            return area;
+
         }
     }
 }
